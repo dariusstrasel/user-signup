2a20dea07d909d26e1783287ec99e6f9bf62f42d
siruku@gmail.com <siruku@gmail.com>
2017-02-05 19:55:31 -0500

    Added manual html for pseudo-shopping list.

--- main.py
+++ main.py
@@ -15,11 +15,62 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader(template_dir))
+
+form_html = """
+<form>
+<h2>Add a food</h2>
+<input type="text" name="food">
+%s
+<button>Add</button>
+</form>
+"""
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+item_html = "<li>%s</li>"
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
+"""
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        output = form_html
+        output_hidden = ""
+
+        items = self.request.get_all("food")
+        if items:
+            output_items = ""
+            for item in items:
+                output_hidden += hidden_html % item
+                output_items += item_html % item
+
+            output_shopping = shopping_list_html % output_items
+            output += output_shopping
+
+        output = output % output_hidden
+
+        self.write(output)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
